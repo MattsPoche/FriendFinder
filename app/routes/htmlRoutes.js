328d482c2049b88f45f398394b72991a9b03daf7
Mattspoche <Matthewspoche@comcast.net>
2018-11-10 17:29:09 -0600

    Added code

---
+++ app/routes/htmlRoutes.js
@@ -0,0 +1,14 @@
+//dependencies
+
+const path = require("path");
+
+//routing
+
+module.exports = function(app) {
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+    app.get("/survey", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+};
--- server.js
+++ server.js
@@ -1,8 +1,20 @@
-var express = require("express");
+const express = require("express");
+const bodyParser = require("body-parser");
 
 var app = express();
 
 const PORT = process.env.PORT || 3000;
+
+//sets up express app to handle data parsing
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
+//Router
+//The below points the server to a series of route files.
+
+require("./app/routes/htmlRoutes")(app);
+
 
 app.get("/", function(req, res){
     res.send("Hello, World!");
