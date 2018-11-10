d35baef0b3fe375e6f8eb997d57765af1ae10a57
Mattspoche <Matthewspoche@comcast.net>
2018-11-10 16:52:07 -0600

    changed txt

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", function(req, res){
-    res.send("hello world");
+    res.send("Hello, World!");
 });
 
 app.listen(PORT, function(){
